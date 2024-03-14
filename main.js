de6b3b50a011b517457a8fb65debcd9515366f76
AHMEDANSARI0 <161446822+AHMEDANSARI0@users.noreply.github.com>
2024-03-14 19:44:37 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+var num1 = 8;
+//Pre Incerement
+console.log(++num1);
+//Post Incerement 
+console.log("line: 5:", num1++);
+console.log("line: 6:", num1);
+//Pre Decrement
+console.log(--num1);
+//Post Decrement
+console.log("line: 10:", num1--);
+console.log("line: 11:", num1);
+//Combining Operator
+var result = 8 + 5 * 2 / 4;
+console.log(result);
+//Comparison Operator 
+var num3 = 6;
+var num4 = 8;
+var equalscomparison = num3 == num4;
+console.log(equalscomparison);
+var notequalscomparison = num3 != num4;
+console.log(notequalscomparison);
+var greaterthan = num3 > num4;
+console.log(greaterthan);
+var lessthan = num3 < num4;
+console.log(lessthan);
